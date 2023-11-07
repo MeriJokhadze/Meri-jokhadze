d53e77c2fc743d3633f8609c8b6c5421a94d10bd
MeriJokhadze <jokhadzemeri1@gmail.com>
2023-11-07 13:19:13 +0400

    city slider is done

--- index.js
+++ index.js
@@ -39,62 +39,128 @@ function citySlider(){
 
   // create data for slider
 
-const georgianCities = [
+let georgianCities = [
   {
+    id: 1,
     title: 'Tbilisi',
     image: '../css/imgs/mainImgs/tbilisi.jpg',
     description: 'Tbilisi, the capital of Georgia, is known for its diverse architecture, historic churches, and vibrant arts scene. It sits on the banks of the Kura River and offers a mix of modern and historic attractions.'
   },
   {
+    id: 2,
     title: 'Batumi',
     image: '../css/imgs/mainImgs/batumi.jpg',
     description: 'Batumi, a seaside city on the Black Sea coast, is famous for its beaches, botanical garden, and modern architecture. It\'s a popular tourist destination with a subtropical climate.'
   },
   {
+    id: 3,
     title: 'Kutaisi',
     image: '../css/imgs/mainImgs/kutaisi.jpg',
     description: 'Kutaisi, Georgia\'s second-largest city, is known for its historical and architectural heritage. It\'s home to the Bagrati Cathedral and the Gelati Monastery, both UNESCO World Heritage Sites.'
   },
-  // {
-  //   title: 'Gori',
-  //   image: '../css/imgs/mainImgs/gori.jpg',
-  //   description: 'Gori is the birthplace of Joseph Stalin and is known for its Stalin Museum. The city is also home to the ancient Uplistsikhe Cave Town and the Gori Fortress.'
-  // },
-  // {
-  //   title: 'Mtskheta',
-  //   image: '../css/imgs/mainImgs/mtskheta.jpg',
-  //   description: 'Mtskheta, one of the oldest cities in Georgia, is a UNESCO World Heritage Site. It\'s known for its historical and religious significance, with attractions like the Svetitskhoveli Cathedral and Jvari Monastery.'
-  // },
-  // {
-  //   title: 'Zugdidi',
-  //   image:  '../css/imgs/mainImgs/samegrelo.jpg',
-  //   description: 'Zugdidi is a city in western Georgia, known for its lush landscapes and the Dadiani Palace, which houses a museum. The city is a gateway to the beautiful Samegrelo region.'
-  // }
+   {
+    id:4,
+    title: 'Gori',
+    image: '../css/imgs/mainImgs/gori.jpg',
+     description: 'Gori is the birthplace of Joseph Stalin and is known for its Stalin Museum. The city is also home to the ancient Uplistsikhe Cave Town and the Gori Fortress.'
+   },
+   {
+    id: 5,
+    title: 'Mtskheta',
+    image: '../css/imgs/mainImgs/mtskheta.jpg',
+    description: 'Mtskheta, one of the oldest cities in Georgia, is a UNESCO World Heritage Site. It\'s known for its historical and religious significance, with attractions like the Svetitskhoveli Cathedral and Jvari Monastery.'
+  },
+   {
+    id: 6,
+     title: 'Zugdidi',
+    image:  '../css/imgs/mainImgs/samegrelo.jpg',
+    description: 'Zugdidi is a city in western Georgia, known for its lush landscapes and the Dadiani Palace, which houses a museum. The city is a gateway to the beautiful Samegrelo region.'
+  }
 ]; 
 
-// create html elements and make containers,
-//  give specific info and return in html
-
 let citiesWrapper = document.querySelector('.cities-wraper');
+let cityNextBtn = document.querySelector('.city-next-btn');
+let cityBackBtn = document.querySelector('.city-back-btn');
 
-georgianCities.forEach((data)=>{
-  let cityBox = document.createElement('div');
-  cityBox.classList.add('box-item');
-  let cityName =document.createElement('h3');
-  cityName.textContent = data.title;
-  let cityImg = document.createElement('img');
-  cityImg.src = data.image;
-  cityImg.classList.add("img-size");
-  let cityInfo = document.createElement('p');
-  cityInfo.textContent = data.description;
+let currentIndex = 0;
+let citiesPerPage = 3; 
 
-  cityBox.appendChild(cityImg);
-  cityBox.appendChild(cityName);
-  cityBox.appendChild(cityInfo);
+function updateCitiesPerPage() {
+  if (window.innerWidth >= 1024) {
+    citiesPerPage = 3;
+  } else if (window.innerWidth >= 480) {
+    citiesPerPage = 2; 
+  } else {
+    citiesPerPage = 1;
+  }
 
-  citiesWrapper.appendChild(cityBox); 
+  displayCities();
+}
+window.addEventListener('resize', updateCitiesPerPage);
+
+updateCitiesPerPage();
+
+//  display cities based on the current index
+function displayCities() {
+  citiesWrapper.innerHTML = ''; // Clear the container
+
+  for (let i = currentIndex; i < currentIndex + citiesPerPage; i++) {
+    if (i < georgianCities.length) {
+      const cityData = georgianCities[i];
+
+      let cityBox = document.createElement('div');
+      cityBox.classList.add('box-item');
+      let cityName = document.createElement('h3');
+      cityName.textContent = cityData.title;
+      let cityImg = document.createElement('img');
+      cityImg.src = cityData.image;
+      cityImg.classList.add("img-size");
+      let cityInfo = document.createElement('p');
+      cityInfo.textContent = cityData.description;
+
+      cityBox.appendChild(cityImg);
+      cityBox.appendChild(cityName);
+      cityBox.appendChild(cityInfo);
+
+      setTimeout(() => {
+        cityBox.classList.add('show');
+      }, 50 * (i - currentIndex));
+
+      citiesWrapper.appendChild(cityBox);
+    }
+  }
+
+  setTimeout(() => {
+    for (let i = 0; i < georgianCities.length; i++) {
+      if (i < currentIndex || i >= currentIndex + citiesPerPage) {
+        const city = citiesWrapper.children[i - currentIndex];
+        city.classList.remove('show');
+      }
+    }
+  }, 100);
+}
+
+// EVENTLISTENERS
+
+cityNextBtn.addEventListener('click', () => {
+  currentIndex += citiesPerPage;
+  if (currentIndex >= georgianCities.length) {
+    currentIndex = 0;
+  }
+  displayCities();
 });
 
+cityBackBtn.addEventListener('click', () => {
+  currentIndex -= citiesPerPage;
+  if (currentIndex < 0) {
+    currentIndex = Math.max(georgianCities.length - citiesPerPage, 0);
+  }
+  displayCities();
+});
+
+//DISPLAY CITIES
+
+displayCities();
 }
 
 // // POP Up
